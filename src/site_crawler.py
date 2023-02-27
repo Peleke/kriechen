1f9ab2a96cb56765aa708bf129e069e710cec495
Peleke <kraken@peleke.33mail.com>
2023-02-26 20:56:03 -0500

    Add empty files for SiteCrawler and its tests.

---
+++ src/site_crawler.py
@@ -0,0 +1,8 @@
+"""SiteCrawler uses Crawler to instantiate Producers and Consumers specifically designed to generate links
+and read their content."""
+from .crawler import Crawler
+
+
+class SiteCrawler:
+    def __init__(self):
+        pass
---
+++ tests/test_site_crawler.py
@@ -0,0 +1,8 @@
+import pytest
+
+from src.site_crawler import SiteCrawler
+
+
+class TestSiteCrawler:
+    def test_site_crawler(self):
+        pytest.fail()
