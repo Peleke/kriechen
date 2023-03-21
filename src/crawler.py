d50ddaa9a7cabc867c8ee55385423d30b50bfe9b
Peleke <kraken@peleke.33mail.com>
2023-03-20 20:19:10 -0400

    Add docstrings to Crawler and Transformer.

--- src/crawler.py
+++ src/crawler.py
@@ -1,6 +1,6 @@
 """Crawler is responsible for instantiating Producers and Consumers, and -- importantly -- managing their execution and
 termination."""
-from typing import Any, Callable, Dict, Tuple
+from typing import Any, Callable, Dict
 import asyncio
 import logging
 
@@ -27,6 +27,41 @@ class Crawler:
         sink_max: int = 10,
         source_max: int = 10,
     ):
+        """Factory method
+
+        The `create` method is used to create a new Crawler and generate its Producers/Consumers in one shot.
+
+        :param base_url: The initial 'seed' URL used to kick off crawling.
+        :type base_url: str
+
+        :param consumer_transformer: The Transformer instance used to create Consumers.
+        :type consumer_transformer: Transformer
+
+        :param producer_transformer: The Transformer instance used to create Producers.
+        :type producer_transformer: Transformer
+
+        :param terminate: The function used to determine when the Crawler should initiate shutdown.
+        :type terminate: Callable
+
+        :param init: Boolean indicating whether to generate Producers/Consumers upon creation.
+        :type init: bool
+
+        :param producer_count: Number of Producers to configure.
+        :type producer_count: int
+
+        :param consumer_count: Number of Consumers to configure.
+        :type consumer_count: int
+
+        :param sink_max: Maximum number of items that cana be placed in underlying `sink` queue.
+        :type sink_max: int
+
+        :param source_max: Maximum number of items that cana be placed in underlying `source` queue.
+        :type source_max: int
+
+        :return: Crawler instance. If `init == True`, the returned instance will have its `producers`
+            and `consumers` properties populated immediately.
+        :rtype: Crawler
+        """
         self = cls(
             base_url=base_url,
             consumer_count=consumer_count,
@@ -54,6 +89,7 @@ class Crawler:
         source_max: int = 10,
         terminate: Callable = (lambda *_: False),
     ):
+        """Constructor method"""
         self.base_url = base_url
         self.max_links = max_links
 
@@ -91,7 +127,12 @@ class Crawler:
     #############
     # THROWAWAY #
     #############
-    def seed_source(self, input_: str):
+    def seed_source(self, input_: str) -> None:
+        """Place an initial 'seed' element onto the source queue to initiate crawling.
+
+        :param input_: Initial element to place on queue.
+        :type input_: str
+        """
         self.source.put_nowait(input_)
 
     ##########################
@@ -99,17 +140,42 @@ class Crawler:
     ##########################
     @property
     def done(self) -> bool:
+        """Boolean representing whether underlying registry is full.
+
+        :return: `True` if `self.registry` is full, `False` otherwise.
+        :rtype: bool
+        """
         return self.registry.full
 
     @property
     def shutting_down(self) -> bool:
+        """Boolean representing whether Crawler is in the process of draining its queues and shutting down.
+
+        :return: `True` if Crawler is draining and shutting down, `False` otherwise.
+        :rtype: bool
+        """
         return self.__shutting_down
 
     def shut_down(self) -> bool:
+        """Method to initiate a shutdown. This flags `self.__shutting_down` as `True`, and begins draining the
+        `self.source` and `self.sink` queues.
+
+        :return: Returns the value of `self.__shutting_down`, which will always be `True` after invoking this method.
+        :rtype: bool
+        """
         self.__shutting_down = True
         return self.__shutting_down
 
     async def update_registry(self, element: Any) -> Any:
+        """Meethod to add an element to the underlying registry and emit an event indicating that the registry has
+        been updated. The event is 'heard' by the Destructor, which initiates shutdown when it is full.
+
+        :param element: Element added to registry.
+        :type element: Any
+
+        :return: Returns the element added to the underlying Registry.
+        :rtype: Any
+        """
         self.registry.add(element)
         self.event_bus.emit(
             CrawlerEvents.Registry.UPDATED,
@@ -124,7 +190,15 @@ class Crawler:
     ###############
     # MAIN METHOD #
     ###############
-    async def crawl(self):
+    async def crawl(self) -> None:
+        """This method initiates the crawling process. It:
+        - Generates a series of Producers and Consumers
+        - Seeds the input queue
+        - Initiates the Producer/Consumer Tasks
+        - Waits for Producers/Consumers to finish, _or_ for Crawler to initiate shutdown
+        - Waits for `Queue.join` to complete
+        - Cancels hanging Consumers
+        """
         # Generate producers/consumers
         producers = await self.generate_producers()
         consumers = await self.generate_consumers()
@@ -152,9 +226,11 @@ class Crawler:
     ##################
     @property
     def producer_tasks(self):
+        """Returns a list of Producer tasks."""
         return [p["task"] for p in self.producers.values()]
 
     async def generate_producers(self):
+        """Generate Producers. These are 'placeholders' used by `crawl` to initiate the actual Production process."""
         while len(self.producers) < self.producer_count:
             self.producer(
                 id=len(self.producers),
@@ -162,7 +238,16 @@ class Crawler:
             )
         return self.producers
 
-    def producer(self, id: int, transformer: Transformer):
+    def producer(self, id: int, transformer: Transformer) -> Producer:
+        """Create a Producer instance using the provided `transformer`. All Producers generated by a given `Crawler`
+        will use the same `transformer`.
+
+        :param transformer: A Transformer instance used to configure the generated Producer.
+        :type transformer: Transformer
+
+        :return: An instance of Producer configured with the provided `transformer`.
+        :rtype: Producer
+        """
         producer = Producer(
             id=id,
             transformer=transformer,
@@ -183,9 +268,11 @@ class Crawler:
     ##################
     @property
     def consumer_tasks(self):
+        """Returns a list of Consumer tasks."""
         return [c["task"] for c in self.consumers.values()]
 
     async def generate_consumers(self):
+        """Generate Consumers. These are 'placeholders' used by `crawl` to initiate the actual Consumption process."""
         while len(self.consumers) < self.consumer_count:
             self.consumer(
                 id=len(self.consumers),
@@ -194,6 +281,15 @@ class Crawler:
         return self.consumers
 
     def consumer(self, id: int, transformer: Transformer):
+        """Create a Consumer instance using the provided `transformer`. All Consumers generated by a given `Crawler`
+        will use the same `transformer`.
+
+        :param transformer: A Transformer instance used to configure the generated Consumer.
+        :type transformer: Transformer
+
+        :return: An instance of Consumer configured with the provided `transformer`.
+        :rtype: Consumer
+        """
         consumer = Consumer(
             id=id,
             event_bus=self.event_bus,
@@ -220,24 +316,51 @@ class Crawler:
             terminate: Callable,
             drain_timeout: int = 1,
         ):
+            """Constructor method
+
+            The `Destructor` must be configured with:
+            - A parent Crawler
+            - The parent Crawler's `event_bus` and `terminate` properties
+            - A `drain_timeout` indicating how long to wait for async `put` methods to compelte before terminating
+              the drain
+
+            The Destructor's methods are invoked via event emissions rather than imperative invocation.
+            """
             self.crawler = crawler
             self.drain_timeout = drain_timeout
             self.event_bus = event_bus
             self.terminate = terminate
 
         async def shutdown(self, _: Any) -> None:
+            """Initiate a shutdown on the parent Crawler by terminating all running workers and draining the
+            underlying queues.
+
+            :param _: A generic parameter used to accept the Event, which, by this implementation, remains unused.
+            :type _: Any
+            """
             self.terminate_workers()
             drain_tasks = [asyncio.create_task(self.drain(q)) for q in ["sink", "source"]]
             self.crawler.shut_down()
             await asyncio.gather(*drain_tasks)
 
         async def watch(self, event: Any) -> None:
-            logging.info("Checking if done...")
+            """A method invoked on eveery `CrawlerEvents.Registry.UPDATED` event, and used to determine if the crawler
+            is done. If so, it emits `CrawlerEvents.Crawler.TERMINATE` to trigger a shutdown on the parent Crawler.
+
+            :param event: The event triggering invocation of `watch`.
+            :type event: Any
+            """
             logging.info(self.crawler.done)
             if self.terminate(self.crawler, event):
                 self.event_bus.emit(CrawlerEvents.Crawler.TERMINATE)
 
         async def drain(self, queue_name: str) -> None:
+            """Removes all items from the underlying sink/soure queues to trigger completion of the `Queue.join`
+            call in the parent Crawler and terminate the run.
+
+            :param queue_name: Name of the queue to drain. Should be either `"source"` or `"sink"`.
+            :type queue_name: str
+            """
             queue = getattr(self.crawler, queue_name)
             while True:
                 try:
--- src/transformer.py
+++ src/transformer.py
@@ -12,6 +12,20 @@ class Transformer:
         fn: Optional[Callable[..., Any]] = None,
         fn_sink: Optional[Callable[..., Any]] = None,
     ):
+        """Constructor method
+
+        :param fn_raw: Callable invoked to transform raw elements taken from queue before passing them to `fn`
+            for processing. Can be used to, e.g., separate data from its priority.
+        :type fn_raw: Callable[..., Any]
+
+        :param fn: Callable invoked to transform an element read from the queue, e.g., fetch the HTML from a URL.
+        :type fn: Callable[..., Any]
+
+        :param fn_sink: Callable invoked to transform the result of processing an element into an output to be placed
+            on the sink queue/fed back into the production/consumption loop. E.g., use `fn` to read all text from a
+            URL, but `fn_sink` to read all URLs from that page and feed them back into the loop.
+        :type fn_sink: Callable[..., Any]
+        """
         self.fn_raw = fn_raw if fn_raw else Transformer.NOOP
         self.fn = fn if fn else Transformer.NOOP
         self.fn_sink = fn_sink if fn_sink else Transformer.NOOP
